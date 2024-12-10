bce3f3aa7d07c5d7a9950e7659d8a7a09897b447
Brohii <81881592+Brohii@users.noreply.github.com>
2024-12-11 00:44:46 +0500

    Update speech.interview.route.js

--- src/routes/speech.interview.route.js
+++ src/routes/speech.interview.route.js
@@ -1,10 +1,10 @@
-import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { interviewSession, speechInterviewSession, startInterviewSession } from "../controllers/speech.interview.controller.js";
+// import { Router } from "express";
+// import { verifyJWT } from "../middlewares/auth.middleware.js";
+// import { interviewSession, speechInterviewSession, startInterviewSession } from "../controllers/speech.interview.controller.js";
 
-const router = Router()
+// const router = Router()
 
-router.route("/start-speech-interview-session").post(verifyJWT, startInterviewSession)
-router.route("/speech-interview-session").get(verifyJWT, speechInterviewSession)
+// router.route("/start-speech-interview-session").post(verifyJWT, startInterviewSession)
+// router.route("/speech-interview-session").get(verifyJWT, speechInterviewSession)
 
-export default router
+// export default router
